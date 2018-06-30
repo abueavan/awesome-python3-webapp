5202434f2822718c056ce1fbebc460e7ada5ce7e
abu <1358950191@qq.com>
2018-06-30 16:10:53 +0800

    update

--- www/handlers.py
+++ www/handlers.py
@@ -3,16 +3,18 @@
 
 __author__ = 'abueavan'
 
+' url handlers '
 
-import asyncio
-from coroweb import get,post
+import re, time, json, logging, hashlib, base64, asyncio
 
-#编写用于测试的URL处理函数
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
 @get('/')
-async def handler_url_blog(request):
-    body='<h1>Awesome</h1>'
-    return body
-@get('/greeting')
-async def handler_url_greeting(*,name,request):
-    body='<h1>Awesome: /greeting %s</h1>'%name
-    return body
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
